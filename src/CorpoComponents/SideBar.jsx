e7c9512dfdf8b8c1d9591fb7c1bbbaed5d251a0f
LivisonVinicius <livisongc@gmail.com>
2022-05-07 17:44:49 -0300

    Todos os requisitos completos

--- src/CorpoComponents/SideBar.jsx
+++ src/CorpoComponents/SideBar.jsx
@@ -3,11 +3,29 @@ export default function SideBar(){
     conta:"catanacomics",
     nome:"Catana"
   }
+  const sugestedUsers = [
+    {conta:"bad.vibes.memes"},
+    {conta:"chibirdart"},
+    {conta:"razoesparaacreditar"},
+    {conta:"adorable_animals"},
+    {conta:"smallcutecats"}
+  ]
+  const SugestoesDeUsuario = sugestedUsers.map(Sugestao => 
+    <div class="sugestao">
+      <Sugestoes conta={Sugestao.conta}/>
+    </div>
+    )
     return (
         <div class="sidebar">
           <Usuario conta={UserAccount.conta} nome={UserAccount.nome} />
 
-          <Sugestoes />
+          <div class="sugestoes">
+            <div class="titulo">
+              Sugestões para você
+              <div>Ver tudo</div>
+            </div>
+            {SugestoesDeUsuario}
+          </div>
 
           <div class="links">
             Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
@@ -20,7 +38,6 @@ export default function SideBar(){
     )
 }
 function Usuario(props){
-  
   return(
     <div class="usuario">
       <img src={`assets/img/${props.conta}.svg`} />
@@ -31,34 +48,13 @@ function Usuario(props){
     </div>
   )
 }
-function Sugestoes(){
-  const sugestedUsers = [
-    "bad.vibes.memes",
-    "chibirdart",
-    "razoesparaacreditar",
-    "adorable_animals",
-    "smallcutecats"
-  ]
-  const sugestion= sugestedUsers.map(Sugestion =>
-    <div class="sugestao">
-      <div class="usuario">
-        <img src={`assets/img/${Sugestion}.svg`} />
-        <div class="texto">
-          <div class="nome">{Sugestion}</div>
-          <div class="razao">Segue você</div>
-        </div>
-      </div>
-      <div class="seguir">Seguir</div>
-    </div>
-  )
+function Sugestoes(props){
   return(
-    <div class="sugestoes">
-      <div class="titulo">
-        Sugestões para você
-        <div>Ver tudo</div>
-      </div>
-      <div class="sugestoes">
-        {sugestion}
+    <div class="usuario">
+      <img src={`assets/img/${props.conta}.svg`} />
+      <div class="texto">
+        <div class="nome">{props.conta}</div>
+        <div class="razao">Segue você</div>
       </div>
     </div>
   )
--- src/CorpoComponents/Stories.jsx
+++ src/CorpoComponents/Stories.jsx
@@ -1,35 +1,36 @@
 
 export default function Stories() {
   const listaDeStories = [
-    "9gag",
-    "meowed",
-    "barked",
-    "nathanwpylestrangeplanet",
-    "wawawicomics",
-    "respondeai",
-    "filomoderna",
-    "memeriagourmet"
-  ]
+    
+    {conta:"9gag"},
+    {conta:"meowed"},
+    {conta:"barked"},
+    {conta:"nathanwpylestrangeplanet"},
+    {conta:"wawawicomics"},
+    {conta:"respondeai"},
+    {conta:"filomoderna"},
+    {conta:"memeriagourmet"}
+  ];
+  const componenteStories = listaDeStories.map(Storie =>
+    <Story conta={Storie.conta}/>)
   return (
     <div class="stories">
-        <Story listaDeStories={listaDeStories}/>
+        {componenteStories}
         <div class="setinha">
           <ion-icon name="chevron-forward-circle"></ion-icon>
         </div>
     </div>
-)
+);
 }
 function Story(props) {
-  const componenteStories = props.map(Storie =>
+  return(
     <div class="story">
       <div class="imagem">
-        <img src={`assets/img/${Storie}.svg`}/>
+        <img src={`assets/img/${props.conta}.svg`}/>
       </div>
       <div class="usuario">
-        {Storie}
+        {props.conta}
       </div>
-    </div>)
-  return(
-    {componenteStories}
+    </div>
   )
 }
